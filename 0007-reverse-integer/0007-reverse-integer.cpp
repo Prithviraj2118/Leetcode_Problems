1a7db777ae53a471008d6437babe98fef5e148b0
Prithviraj Amol Raut <136347677+Prithviraj2118@users.noreply.github.com>
2023-09-19 23:24:20 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (7.54%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long reverse = 0, rem;
+        bool f = false;
+        if(x==-2147483648) return 0;
+        
+        if(x<0){
+            f = true;
+            x = -x;
+        }
+        while (x != 0) {
+            rem = x% 10;
+            reverse = reverse * 10 + rem;
+            x /= 10;
+        }
+        if(reverse > 2147483648) return 0;
+        
+        if(f==true) return -reverse;
+        return reverse;
+    }
+};
