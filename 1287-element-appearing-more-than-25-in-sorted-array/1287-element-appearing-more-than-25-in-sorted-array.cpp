4bce79d0fda184ed13a3bf8507a553c561f0c9cc
Prithviraj Amol Raut <136347677+Prithviraj2118@users.noreply.github.com>
2024-01-08 19:57:43 +0530

    Time: 7 ms (80.19%), Space: 12.8 MB (44.38%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
-        int occ = arr.size()/4;
-        unordered_map<int, int> m;
+        int i=0, j=0;   //Window start and end pointer
         
-        for(int i=0; i<arr.size(); i++){
-            m[arr[i]]++;
-            if(m[arr[i]]>occ) return arr[i];
+        while(j<arr.size()){
+            while(j<arr.size() && arr[i]==arr[j]){
+                j++;
+            }
+            if((j-i) > arr.size()/4) return arr[i];  //If element occurs > 25% array
+            i=j;    //Update window start
         }
-        return -1;
+        return -1;  //No element occurs > 25%
     }
 };
