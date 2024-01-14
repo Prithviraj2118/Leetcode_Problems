684b80f2f7d6e63652f67e1c1e2129ac6d7778ae
Prithviraj Amol Raut <136347677+Prithviraj2118@users.noreply.github.com>
2024-01-14 11:55:03 +0530

    Time: 81 ms (37.45%), Space: 17 MB (36.12%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -5,24 +5,23 @@ public:
         int cnt=0;
         
         for(auto x:s){
-            m1[x]++;
+            m1[x]++; //Stor string s chars in map m1
         }
         
         for(int i=0; i<t.size(); i++){
             if(m1[t[i]]>1){
                 m1[t[i]]--;
-                t[i]='.';
+                t[i]='.';   //Replace the current char by '.'
+                cnt++;
             }
             
             else if(m1[t[i]]==1) {
                 m1.erase(t[i]);
-                t[i]='.';
+                t[i]='.';   //Replace the current char by '.'
+                cnt++;
             }
         }
         
-        for(auto x:t){
-            if(x!='.') cnt++;
-        }
-        return cnt;
+        return t.size()-cnt;
     }
 };
