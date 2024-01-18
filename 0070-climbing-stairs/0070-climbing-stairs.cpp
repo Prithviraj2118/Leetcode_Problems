d103cca5a79ab351235a121e67a41d64c6cdb0f6
Prithviraj Amol Raut <136347677+Prithviraj2118@users.noreply.github.com>
2024-01-18 12:12:01 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (59.54%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(vector<int> &dp, int i, int n){
+        if(i==n) return 1;
+        else if(i>n) return 0;
+        
+        if(dp[i] != -1) return dp[i];
+        
+        int a = solve(dp, i+1, n); //1 step
+        int b = solve(dp, i+2, n);  //2 step
+            
+        return dp[i]=a+b; //Totsl ways
+    }
+    
+    int climbStairs(int n) {
+        vector<int> dp(n, -1);
+        return solve(dp, 0, n);
+    }
+};
