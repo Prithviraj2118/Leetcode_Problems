f5f082872a7aca5cd2fc870d4336bcefbb993226
Prithviraj Amol Raut <136347677+Prithviraj2118@users.noreply.github.com>
2024-02-02 10:48:27 +0530

    Time: 165 ms (5.09%), Space: 15.1 MB (18.04%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        long long cnt_1 = 0, cnt_swap = 0;  //no.of ones
-        for(auto x:s){
-            if(x=='1') cnt_1++;
-            else cnt_swap += cnt_1; // if x==0
+        long long cnt_1 = 0, cnt_0 = 0, cnt_swap=0;  //no.of ones
+        int i=0;
+        while(i<s.size()){
+            if(s[i]=='1'){ 
+                cnt_1++;
+                i++;
+            }
+            else{
+                while(i<s.size() && s[i]=='0'){
+                    cnt_0++;
+                    i++;
+                }
+                cout << cnt_1 << " " << cnt_0 << endl;
+                cnt_swap += cnt_1*cnt_0; // if x==0
+                cnt_0=0;
+            }
         }
         return cnt_swap;
     }
