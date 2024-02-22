7a36ce20d80bb4a8c3d6517001deda2270c568f0
Prithviraj Amol Raut <136347677+Prithviraj2118@users.noreply.github.com>
2024-02-22 10:36:24 +0530

    Time: 162 ms (28.57%), Space: 74.1 MB (12.54%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         if(n==1) return 1;
-        unordered_map<int, int> m1, m2;
+        unordered_map<int, int> m1;
+        unordered_set<int> s;
         int mx = 0, mx_el;
         for(auto x:trust){
-            m2[x[0]]++;
+            s.insert(x[0]);
             m1[x[1]]++;
             if(mx < m1[x[1]]){
                 mx = m1[x[1]];
                 mx_el = x[1];
             }
         }
-        if(mx==n-1 && m2.find(mx_el)==m2.end()) return mx_el;
+        if(mx==n-1 && s.find(mx_el)==s.end()) return mx_el;
         return -1;
     }
 };
