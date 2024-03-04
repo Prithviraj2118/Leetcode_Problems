b8d73a091c5a31ed3ed07b40cc8a8493d78e857f
Prithviraj Amol Raut <136347677+Prithviraj2118@users.noreply.github.com>
2024-03-04 22:53:07 +0530

    Time: 219 ms (71.43%), Space: 98.6 MB (42.86%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -7,15 +7,12 @@ public:
         for(auto x:nums){
             pq.push(-x);
         }
-        if(pq.size()<=1) return 0;
         while(-pq.top()<k && pq.size()>=2){
-            a = -pq.top();
-            pq.pop();
-            b = -pq.top();
-            pq.pop();
+            a = -pq.top(), pq.pop();
+            b = -pq.top(), pq.pop();
             c = min(a, b)*2 + max(a,b);
-            cnt++;
             pq.push(-c);
+            cnt++;
         }
         
         return cnt;
