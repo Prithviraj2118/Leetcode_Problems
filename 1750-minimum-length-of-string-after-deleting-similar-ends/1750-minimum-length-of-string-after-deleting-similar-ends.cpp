8eabef1863b1e96d16a0e3a85db47b5c14858413
Prithviraj Amol Raut <136347677+Prithviraj2118@users.noreply.github.com>
2024-03-08 22:45:42 +0530

    Time: 30 ms (23.02%), Space: 13.9 MB (81.45%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int i=0, j=s.size()-1;
+        if(s.length()==1) return 1;
+        if(s.length()==2){
+            if(s[i]==s[j]) return 0;
+            else return 2;
+        }
+        while(i<j-1 && s[i]==s[j]){
+            while(i<s.size() && s[i]==s[j]) i++;
+            while(j>=0 && s[j]==s[i-1]) j--;
+        }
+        if(i>j) return 0;
+        if(j==i+1){
+            if(s[i]==s[j]) return 0;
+            else return 2;
+        }
+        return j-i+1;
+    }
+};
